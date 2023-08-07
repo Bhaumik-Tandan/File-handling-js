0dfeff412919f741dbe7965cb490c4b198e9a3ad
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-08-07 16:49:38 +0530

    [FIX] file import

--- sendingFile.js
+++ sendingFile.js
@@ -1,7 +1,7 @@
 // Sending a text file to the server using Fetch API
 async function sendTextFile() {
     const fileContent = 'This is a sample text content.';
-    const url = 'https://example.com/upload'; // Replace with your server endpoint
+    const url = 'https://webhook.site/23148312-e27e-49a4-a782-d393094b3b13'; // Replace with your server endpoint
 
     try {
         const response = await fetch(url, {
@@ -25,35 +25,3 @@ async function sendTextFile() {
 sendTextFile();
 
 
-// Uploading a binary file to the server using Fetch API
-async function uploadBinaryFile() {
-    const fileInput = document.getElementById('fileInput'); // Input element with type="file"
-    const file = fileInput.files[0]; // Get the selected file
-    const url = 'https://example.com/upload'; // Replace with your server endpoint
-
-    if (!file) {
-        console.error('No file selected.');
-        return;
-    }
-
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': file.type // Set the appropriate MIME type for the file
-            },
-            body: file
-        });
-
-        if (response.ok) {
-            console.log('Binary file uploaded successfully.');
-        } else {
-            console.error('Failed to upload binary file.');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-// Attach the event listener to the file input element
-document.getElementById('fileInput').addEventListener('change', uploadBinaryFile);
